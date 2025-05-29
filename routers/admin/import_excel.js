5a973b3e98cc659b50ec5a75e68d17d8d3a99c51
DOng <khanhdong1702pro@gmail.com>
2025-05-29 22:12:50 +0700

    update import excel variant

--- routers/admin/import_excel.js
+++ routers/admin/import_excel.js
@@ -5,6 +5,7 @@ const ExcelJS = require('exceljs');
 
 // Nếu bạn dùng Sequelize
 const Product = require('../../models/product'); // thay bằng model thực tế
+const Variant = require('../../models/variant')
 
 // Cấu hình multer – lưu tạm file lên ổ đĩa
 const upload = multer({ dest: 'uploads/' });
@@ -33,6 +34,40 @@ router.post('/import-excel', upload.single('file'), async (req, res) => {
         for (const item of imported) {
             console.log(item);
             await Product.create(item);
+        }
+
+        res.json({ message: 'Import thành công', count: imported.length });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Lỗi khi import dữ liệu' });
+    }
+});
+
+
+router.post('/import-excel-variants', upload.single('file'), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ error: 'Chưa chọn file Excel' });
+        }
+        console.log(req.file);
+
+        const workbook = new ExcelJS.Workbook();
+        await workbook.xlsx.readFile(req.file.path);
+
+        const worksheet = workbook.getWorksheet(1); // sheet đầu tiên
+        const imported = [];
+
+        worksheet.eachRow((row, rowNumber) => {
+            if (rowNumber === 1) return; // bỏ qua dòng tiêu đề
+
+            const [stt, name, price, product_id] = row.values.slice(1); // bỏ index 0
+            imported.push({ name, price, product_id });
+        });
+
+        // Lưu vào DB (giả sử bạn dùng Sequelize)
+        for (const item of imported) {
+        
+            await Variant.create(item);
         }
 
         res.json({ message: 'Import thành công', count: imported.length });
